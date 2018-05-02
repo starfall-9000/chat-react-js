940b75045a44663097491faa65bb2db956ceb2fb
anvu-trustcircle <an.vu@trustcircle.com>
2018-05-02 18:36:20 +0700

    add server

---
+++ server/app.js
@@ -0,0 +1,66 @@
+const WebSocket = require('ws')
+
+const PORT = process.env.PORT || 8989
+const wss = new WebSocket.Server({ port: PORT })
+
+console.log(`Listening on ${PORT}`)
+
+const users = []
+
+const broadcast = (data, ws) => {
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN && client !== ws) {
+      client.send(JSON.stringify(data))
+    }
+  })
+}
+
+wss.on('connection', ws => {
+  let index
+  ws.on('message', message => {
+    const data = JSON.parse(message)
+    switch (data.type) {
+      case 'ADD_USER': {
+        index = users.length
+        users.push({ name: data.name, id: index + 1 })
+        ws.send(
+          JSON.stringify({
+            type: 'USERS_LIST',
+            users
+          })
+        )
+        broadcast(
+          {
+            type: 'USERS_LIST',
+            users
+          },
+          ws
+        )
+        break
+      }
+      case 'ADD_MESSAGE':
+        broadcast(
+          {
+            type: 'ADD_MESSAGE',
+            message: data.message,
+            author: data.author
+          },
+          ws
+        )
+        break
+      default:
+        break
+    }
+  })
+
+  ws.on('close', () => {
+    users.splice(index, 1)
+    broadcast(
+      {
+        type: 'USERS_LIST',
+        users
+      },
+      ws
+    )
+  })
+})
